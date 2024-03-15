8db1e0eff2c74801ade94af4fafa350f8cd7aa63
tracy8 <tracymurenzi@gmail.com>
2024-03-15 15:28:32 +0200

    output

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+function that writes a string to a text file (UTF8)
+"""
+
+
+def write_file(filename="", text=""):
+    """ module write_file
+    """
+    with open(filename, 'w') as f:
+        return f.write(text)

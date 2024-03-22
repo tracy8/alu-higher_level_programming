c186ddcd0c060c0c7fedff0f989296050fe87adf
tracy8 <tracymurenzi@gmail.com>
2024-03-22 15:29:41 +0200

    fix

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -9,7 +9,7 @@ import urllib.request
 
 if __name__ == '__main__':
     with urllib.request.urlopen('https://alu-intranet.hbtn.io/status') as res:
-        content = res.read()
+        content = (res.read) 
         print("Body response:")
         print("\t- type: {}".format(type(content)))
         print("\t- content: {}".format(content))
